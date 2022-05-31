019c049dccc44e66384833dbd1d376d2cc966cd5
Filip <filip26@gmail.com>
2022-05-31 22:46:16 +0200

    Improve Vc.verify implementation

--- src/main/java/com/apicatalog/vc/Vc.java
+++ src/main/java/com/apicatalog/vc/Vc.java
@@ -5,6 +5,8 @@ import java.net.URI;
 import com.apicatalog.jsonld.JsonLd;
 import com.apicatalog.jsonld.JsonLdError;
 import com.apicatalog.jsonld.document.Document;
+import com.apicatalog.jsonld.document.JsonDocument;
+import com.apicatalog.jsonld.json.JsonUtils;
 import com.apicatalog.jsonld.loader.DocumentLoader;
 import com.apicatalog.jsonld.loader.DocumentLoaderOptions;
 import com.apicatalog.lds.DataIntegrityError;
@@ -18,6 +20,7 @@ import com.apicatalog.lds.proof.ProofOptions;
 
 import jakarta.json.JsonArray;
 import jakarta.json.JsonObject;
+import jakarta.json.JsonValue;
 
 /**
  * High level API to process Verified Credentials and Verified Presentations.
@@ -37,10 +40,55 @@ public final class Vc {
         /* FIXME use VerificationApi, make loader optional - use default */
         try {
             // load the document
-            final JsonObject document = JsonLd.expand(location).loader(loader).get().getJsonObject(0); // FIXME
+            final JsonArray expanded = JsonLd.expand(location).loader(loader).get();
 
+            return verifyExpanded(expanded, loader);
+            
+        } catch (JsonLdError e) {
+            throw new VerificationError(e);
+        }
+    }
+
+    /**
+     * Verifies VC/VP document data integrity and signature.
+     * 
+     * @param location
+     * @param loader
+     * @throws DataIntegrityError
+     * @throws VerificationError
+     */
+    public static boolean verify(JsonObject document, DocumentLoader loader) throws DataIntegrityError, VerificationError {
+        /* FIXME use VerificationApi, make loader optional - use default */
+        try {
+            // load the document
+            final JsonArray expanded = JsonLd.expand(JsonDocument.of(document)).loader(loader).get();
+            
+            return verifyExpanded(expanded, loader);
+
+        } catch (JsonLdError e) {
+            throw new VerificationError(e);
+        }
+    }
+    
+    private static boolean verifyExpanded(JsonArray expanded, DocumentLoader loader) throws DataIntegrityError, VerificationError {
+        //TODO validate each objects ?!
+        for (final JsonValue item : expanded) {
+            if (JsonUtils.isNotObject(item)) {
+                return false;
+            }
+            boolean result = verifyExpanded(item.asJsonObject(), loader);
+            if (!result) {
+                return false;
+            }
+        }        
+        return true;
+    }
+
+    private static boolean verifyExpanded(JsonObject expanded, DocumentLoader loader) throws DataIntegrityError, VerificationError {
+        
+        try {
             // data integrity check
-            EmbeddedProof proof = EmbeddedProof.from(document, loader);
+            EmbeddedProof proof = EmbeddedProof.from(expanded, loader);
 
             // check proof type
             if (!Ed25519Signature2020.TYPE.equals(proof.getType())) {
@@ -51,7 +99,7 @@ public final class Vc {
 
             LinkedDataSignature signature = new LinkedDataSignature(new Ed25519Signature2020());
 
-            return signature.verify(document, verificationMethod, proof.getValue());
+            return signature.verify(expanded, verificationMethod, proof.getValue());
 
         } catch (JsonLdError e) {
             throw new VerificationError(e);
