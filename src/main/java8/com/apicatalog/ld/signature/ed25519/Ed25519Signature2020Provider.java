9387a2d3974628fb668bf5080bbccc5e795dc274
Filip <filip26@gmail.com>
2022-07-02 16:19:15 +0200

    Fix JRE8 build to reflect new packaging structure

--- src/main/java8/com/apicatalog/ld/signature/ed25519/Ed25519Signature2020Provider.java
+++ src/main/java8/com/apicatalog/ld/signature/ed25519/Ed25519Signature2020Provider.java
@@ -1,8 +1,60 @@
 package com.apicatalog.ld.signature.ed25519;
 
-public final class Ed25519Signature2020Provider extends NativeSignatureProvider {
+import java.security.GeneralSecurityException;
 
-    public Ed25519Signature2020Provider() {
-        super("Ed25519");
+import com.apicatalog.ld.signature.KeyGenError;
+import com.apicatalog.ld.signature.SigningError;
+import com.apicatalog.ld.signature.VerificationError;
+import com.apicatalog.ld.signature.VerificationError.Code;
+import com.apicatalog.ld.signature.algorithm.SignatureAlgorithm;
+
+public final class Ed25519Signature2020Provider implements SignatureAlgorithm {
+
+    @Override
+    public void verify(byte[] publicKey, byte[] signature, byte[] data) throws VerificationError {
+        try {
+            SignatureConfig.register();
+
+            Ed25519Verify verifier = new Ed25519Verify(publicKey);
+
+            verifier.verify(signature, data);
+
+        } catch (GeneralSecurityException e) {
+            throw new VerificationError(Code.InvalidSignature, e);
+        }
+    }
+
+    @Override
+    public byte[] sign(byte[] privateKey, byte[] data) throws SigningError {
+
+        try {
+            // Register all signature key types with the Tink runtime.
+            SignatureConfig.register();
+
+            Ed25519Sign signer = new Ed25519Sign(privateKey);
+
+            byte[] signature = signer.sign(data);
+
+            return signature;
+
+        } catch (GeneralSecurityException e) {
+            throw new SigningError(e);
+        }
+    }
+
+    @Override
+    public KeyPair keygen(int length) throws KeyGenError {
+
+        try {
+            Ed25519Sign.KeyPair kp = Ed25519Sign.KeyPair.newKeyPair();
+
+            byte[] privateKey = kp.getPrivateKey();
+            byte[] publicKey = kp.getPublicKey();
+
+            return new KeyPair(publicKey, privateKey);
+
+        } catch (GeneralSecurityException e) {
+            throw new KeyGenError(e);
+        }
     }
 }
