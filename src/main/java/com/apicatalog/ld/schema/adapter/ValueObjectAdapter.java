0072cf2b89e98401e0af591bdb56a03d3c398faf
Filip <filip26@gmail.com>
2022-12-07 18:25:28 +0100

    Fix value object @type validation

--- src/main/java/com/apicatalog/ld/schema/adapter/ValueObjectAdapter.java
+++ src/main/java/com/apicatalog/ld/schema/adapter/ValueObjectAdapter.java
@@ -1,11 +1,13 @@
 package com.apicatalog.ld.schema.adapter;
 
+import com.apicatalog.jsonld.json.JsonUtils;
 import com.apicatalog.jsonld.lang.Keywords;
 import com.apicatalog.jsonld.lang.ValueObject;
 import com.apicatalog.ld.schema.LdTerm;
 
 import jakarta.json.Json;
 import jakarta.json.JsonObjectBuilder;
+import jakarta.json.JsonString;
 import jakarta.json.JsonValue;
 
 public class ValueObjectAdapter implements LdValueAdapter<JsonValue, JsonValue> {
@@ -27,9 +29,19 @@ public class ValueObjectAdapter implements LdValueAdapter<JsonValue, JsonValue> 
             throw new IllegalArgumentException();
         }
 
-        // TODO check type
+        if (type == null) {
+            return value.asJsonObject().get(Keywords.VALUE);            
+        }
 
-        return value.asJsonObject().get(Keywords.VALUE);
+        for (final JsonValue valueType : JsonUtils.toCollection( value.asJsonObject().get(Keywords.TYPE))) {
+
+            if (JsonUtils.isString(valueType) 
+                    && type.uri().equalsIgnoreCase(((JsonString)valueType).getString())) {
+                return value.asJsonObject().get(Keywords.VALUE);
+            }
+        }
+        
+        throw new IllegalArgumentException("The value object @type is not [" + type.uri() +  "] but [" + value.asJsonObject().get(Keywords.TYPE) + "].");
     }
 
     @Override
