e9304eb0e3a48a8e82f6988e6267509c6ef7af8f
Filip <filip26@gmail.com>
2023-07-15 01:00:47 +0200

    Add alternative createDraft method

--- src/main/java/com/apicatalog/vc/integrity/DataIntegritySuite.java
+++ src/main/java/com/apicatalog/vc/integrity/DataIntegritySuite.java
@@ -1,5 +1,10 @@
 package com.apicatalog.vc.integrity;
 
+import java.net.URI;
+import java.time.Instant;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import com.apicatalog.jsonld.json.JsonUtils;
 import com.apicatalog.jsonld.schema.LdObject;
 import com.apicatalog.jsonld.schema.LdProperty;
@@ -7,6 +12,7 @@ import com.apicatalog.jsonld.schema.LdSchema;
 import com.apicatalog.jsonld.schema.LdTerm;
 import com.apicatalog.ld.DocumentError;
 import com.apicatalog.ld.signature.CryptoSuite;
+import com.apicatalog.ld.signature.VerificationMethod;
 import com.apicatalog.vc.VcVocab;
 import com.apicatalog.vc.model.Proof;
 import com.apicatalog.vc.suite.SignatureSuite;
@@ -16,9 +22,9 @@ import jakarta.json.JsonString;
 import jakarta.json.JsonValue;
 
 public class DataIntegritySuite implements SignatureSuite {
-    
+
     protected static final String PROOF_TYPE_NAME = "DataIntegrityProof";
-    
+
     protected static final String PROOF_TYPE_ID = VcVocab.SECURITY_VOCAB + PROOF_TYPE_NAME;
 
     protected final LdSchema methodSchema;
@@ -55,11 +61,46 @@ public class DataIntegritySuite implements SignatureSuite {
     public Proof readProof(JsonObject expanded) throws DocumentError {
 
         final LdSchema proofSchema = DataIntegritySchema.getProof(
-                LdTerm.create(PROOF_TYPE_NAME, VcVocab.SECURITY_VOCAB ),
+                LdTerm.create(PROOF_TYPE_NAME, VcVocab.SECURITY_VOCAB),
                 DataIntegritySchema.getEmbeddedMethod(methodSchema),
                 proofValueSchema);
 
         LdObject ldProof = proofSchema.read(expanded);
+
+        return new DataIntegrityProof(this, proofSchema, ldProof, expanded);
+    }
+
+    public DataIntegrityProof createDraft(
+            VerificationMethod method,
+            URI purpose,
+            Instant created) throws DocumentError {
+        return createDraft(method, purpose, created, null, null);
+    }
+
+    public DataIntegrityProof createDraft(
+            VerificationMethod method,
+            URI purpose,
+            Instant created,
+            String domain,
+            String challenge) throws DocumentError {
+
+        Map<String, Object> proof = new LinkedHashMap<>();
+
+        proof.put(LdTerm.TYPE.uri(), URI.create(PROOF_TYPE_ID));
+        proof.put(DataIntegritySchema.CREATED.uri(), created);
+        proof.put(DataIntegritySchema.PURPOSE.uri(), purpose);
+        proof.put(DataIntegritySchema.VERIFICATION_METHOD.uri(), method);
+        proof.put(DataIntegritySchema.DOMAIN.uri(), domain);
+        proof.put(DataIntegritySchema.CHALLENGE.uri(), challenge);
+
+        final LdObject ldProof = new LdObject(proof);
+
+        final LdSchema proofSchema = DataIntegritySchema.getProof(
+                LdTerm.create(PROOF_TYPE_NAME, VcVocab.SECURITY_VOCAB),
+                DataIntegritySchema.getEmbeddedMethod(methodSchema),
+                proofValueSchema);
+
+        final JsonObject expanded = proofSchema.write(ldProof);
 
         return new DataIntegrityProof(this, proofSchema, ldProof, expanded);
     }
