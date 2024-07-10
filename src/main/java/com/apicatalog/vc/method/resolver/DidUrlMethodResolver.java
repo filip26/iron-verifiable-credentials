ce99812fc0d261e947315cba79ce255b65cd866d
Filip <filip26@gmail.com>
2024-07-10 22:46:39 +0200

    Adopt new DidKey approach

--- src/main/java/com/apicatalog/vc/method/resolver/DidUrlMethodResolver.java
+++ src/main/java/com/apicatalog/vc/method/resolver/DidUrlMethodResolver.java
@@ -12,6 +12,7 @@ import com.apicatalog.ld.DocumentError;
 import com.apicatalog.ld.DocumentError.ErrorType;
 import com.apicatalog.ld.signature.VerificationMethod;
 import com.apicatalog.multibase.MultibaseDecoder;
+import com.apicatalog.multicodec.Multicodec;
 import com.apicatalog.multicodec.MulticodecDecoder;
 import com.apicatalog.multikey.MultiKey;
 import com.apicatalog.multikey.MultiKeyAdapter;
@@ -20,9 +21,11 @@ import com.apicatalog.vc.proof.Proof;
 public class DidUrlMethodResolver implements MethodResolver {
 
     final DidResolver resolver;
+    final MulticodecDecoder codecs;
 
-    public DidUrlMethodResolver(MultibaseDecoder bases, MulticodecDecoder codecs) {
-        resolver = new DidKeyResolver(bases, codecs);
+    public DidUrlMethodResolver(final MultibaseDecoder bases, final MulticodecDecoder codecs) {
+        this.resolver = new DidKeyResolver(bases);
+        this.codecs = codecs;
     }
 
     @Override
@@ -33,7 +36,7 @@ public class DidUrlMethodResolver implements MethodResolver {
 
             return didDocument
                     .verificationMethod().stream()
-                    .map(DidUrlMethodResolver::from)
+                    .map(did -> from(did, codecs))
                     .findFirst()
                     .orElseThrow(() -> new DocumentError(ErrorType.Unknown, "ProofVerificationMethod"));
         } catch (IllegalArgumentException e) {
@@ -46,12 +49,20 @@ public class DidUrlMethodResolver implements MethodResolver {
         return DidUrl.isDidUrl(uri);
     }
 
-    public static final MultiKey from(DidVerificationMethod did) {
+    public static final MultiKey from(DidVerificationMethod did, final MulticodecDecoder codecs) {
         final MultiKey multikey = new MultiKey();
         multikey.setId(did.id().toUri());
         multikey.setController(did.controller().toUri());
-        multikey.setAlgorithm(MultiKeyAdapter.getAlgorithmName(did.codec()));        
-        multikey.setPublicKey(did.publicKey());
+        
+        //TODO improve
+        final Multicodec codec = codecs.getCodec(did.publicKey()).orElseThrow(IllegalArgumentException::new);
+        
+        if (!codec.isEncoded(did.publicKey())) {
+            throw new IllegalArgumentException("Unsupported key encoding [" + did + "].");
+        }
+
+        multikey.setAlgorithm(MultiKeyAdapter.getAlgorithmName(codec));
+        multikey.setPublicKey(codec.decode(did.publicKey()));
         return multikey;
     }
 }
